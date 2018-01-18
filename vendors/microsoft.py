aac68787671a1472cba36a81264117435c2a68eb
Ivan Borko <ivan.borko@styria.hr>
2018-01-18 15:13:38 +0100

    fix microsoft vision API URL

--- vendors/microsoft.py
+++ vendors/microsoft.py
@@ -1,9 +1,10 @@
 import json
 import requests
 
+
 def call_vision_api(image_filename, api_keys):
     api_key = api_keys['microsoft']
-    post_url = "https://api.projectoxford.ai/vision/v1.0/analyze?visualFeatures=Categories,Tags,Description,Faces,ImageType,Color,Adult&subscription-key=" + api_key
+    post_url = "https://westcentralus.api.cognitive.microsoft.com/vision/v1.0/analyze?visualFeatures=Categories,Tags,Description,Faces,ImageType,Color,Adult&subscription-key=" + api_key
 
     image_data = open(image_filename, 'rb').read()
     result = requests.post(post_url, data=image_data, headers={'Content-Type': 'application/octet-stream'})
