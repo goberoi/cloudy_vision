3ddeb1d55119af962048b17cf4631ba856c218b3
Christian Roy <roychristian@gmail.com>
2018-08-05 15:32:39 -0400

    Add support for Google's dominant colors annotations

--- vendors/google.py
+++ vendors/google.py
@@ -41,6 +41,10 @@ def call_vision_api(image_filename, api_keys):
               "type": "SAFE_SEARCH_DETECTION",
               "maxResults": 10
             },
+            {
+              "type": "IMAGE_PROPERTIES",
+              "maxResults": 10
+            },
           ]
         }
       ]
@@ -70,5 +74,11 @@ def get_standardized_result(api_result):
         output['logo_tags'] = []
         for annotation in api_result['logoAnnotations']:
             output['logo_tags'].append((annotation['description'], annotation['score']))
+
+    if 'imagePropertiesAnnotation' in api_result and 'dominantColors' in api_result['imagePropertiesAnnotation']:
+        output['colors'] = []
+        for color_annotation in api_result['imagePropertiesAnnotation']['dominantColors']['colors']:
+            color = "{0:x}".format(int(color_annotation['color']['red'])) + "{0:x}".format(int(color_annotation['color']['green'])) + "{0:x}".format(int(color_annotation['color']['blue']))
+            output['colors'].append((color, color_annotation['score']))
 
     return output
