61180147258b00b6e7c462623053fcba282a2713
Christian Roy <roychristian@gmail.com>
2018-08-10 11:07:35 -0400

    Upgrade IBM vendor implementation, add language and color support

--- vendors/ibm.py
+++ vendors/ibm.py
@@ -1,19 +1,26 @@
 from watson_developer_cloud import VisualRecognitionV3
 
 def call_vision_api(image_filename, api_keys):
-    api_key = api_keys['ibm']
+    api_key = api_keys['ibm']['api_key']
+    iam_url = api_keys['ibm']['url']
+    language = api_keys['ibm']['language']
 
-    # Via example found here:
-    # https://github.com/watson-developer-cloud/python-sdk/blob/master/examples/visual_recognition_v3.py
-    visual_recognition = VisualRecognitionV3('2016-05-20', api_key=api_key)
+    # Via example found in the code snippets from the Implementation doc
+    # in the IBM Watson dashboard/console
+    visual_recognition = VisualRecognitionV3('2018-03-19',
+        iam_api_key=api_key,
+        url=iam_url)
 
     with open(image_filename, 'rb') as image_file:
-        result = visual_recognition.classify(images_file=image_file)
+        result = visual_recognition.classify(images_file=image_file, headers={'Accept-Language': language})
 
     return result
 
 
 def get_standardized_result(api_result):
+    color_indicators = ['color', 'couleur']
+        # add color word in other languages if you use a different
+        # language, as per the configs in api_keys
     output = {
         'tags' : [],
     }
@@ -27,5 +34,19 @@ def get_standardized_result(api_result):
         api_result = api_result["classifiers"][0]
         for tag_data in api_result['classes']:
             output['tags'].append((tag_data['class'], tag_data['score']))
+
+        # some tags contain the word "color", so we use that indicator
+        # to extract color information. This is not as reliable as other
+        # vendors providing structured information about colors, but it
+        # is better than nothing
+        color_tags = [tag for tag in api_result['classes'] for color_indicator in color_indicators if color_indicator in tag['class']]
+        if len(color_tags) > 0:
+            output['colors'] = []
+            for color_tag in color_tags:
+                color = color_tag['class']
+                for color_indicator in color_indicators:
+                    color = color.replace(color_indicator, '')
+                color = color.strip()
+                output['colors'].append((color, color_tag['score']))
 
     return output
